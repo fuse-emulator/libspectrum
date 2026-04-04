19fb7610c48fdf202dfc35e8cfa7ca4ac70bc65b
Fredrick Meunier <fredm@spamcop.net>
2026-04-05 08:01:11 +1000

    Add buffer write function tests
    Add six new tests covering the libspectrum_buffer write API:
    - test_77: write_byte stores a single byte correctly
    - test_78: write_word stores a 16-bit value little-endian
    - test_79: write_dword stores a 32-bit value little-endian
    - test_80: write_buffer copies source contents to destination
    - test_81: is_empty / is_not_empty reflect write state correctly
    - test_82: buffer_clear resets data size to zero

--- test/test.c
+++ test/test.c
@@ -1015,6 +1015,213 @@ test_76( void )
 #endif
 }
 
+/* Buffer write tests */
+
+static test_return_t
+test_77( void )
+{
+  /* libspectrum_buffer_write_byte: single byte stored correctly */
+  libspectrum_buffer *buf = libspectrum_buffer_alloc();
+  test_return_t r = TEST_FAIL;
+  const libspectrum_byte *data;
+
+  libspectrum_buffer_write_byte( buf, 0xab );
+
+  if( libspectrum_buffer_get_data_size( buf ) != 1 ) {
+    fprintf( stderr, "%s: test_77: expected size 1, got %lu\n", progname,
+             (unsigned long)libspectrum_buffer_get_data_size( buf ) );
+    goto done;
+  }
+
+  data = libspectrum_buffer_get_data( buf );
+  if( data[0] != 0xab ) {
+    fprintf( stderr, "%s: test_77: expected 0xab, got 0x%02x\n",
+             progname, data[0] );
+    goto done;
+  }
+
+  r = TEST_PASS;
+
+done:
+  libspectrum_buffer_free( buf );
+  return r;
+}
+
+static test_return_t
+test_78( void )
+{
+  /* libspectrum_buffer_write_word: value stored little-endian */
+  libspectrum_buffer *buf = libspectrum_buffer_alloc();
+  test_return_t r = TEST_FAIL;
+  const libspectrum_byte *data;
+
+  libspectrum_buffer_write_word( buf, 0x1234 );
+
+  if( libspectrum_buffer_get_data_size( buf ) != 2 ) {
+    fprintf( stderr, "%s: test_78: expected size 2, got %lu\n", progname,
+             (unsigned long)libspectrum_buffer_get_data_size( buf ) );
+    goto done;
+  }
+
+  data = libspectrum_buffer_get_data( buf );
+  if( data[0] != 0x34 || data[1] != 0x12 ) {
+    fprintf( stderr, "%s: test_78: expected 0x34 0x12, got 0x%02x 0x%02x\n",
+             progname, data[0], data[1] );
+    goto done;
+  }
+
+  r = TEST_PASS;
+
+done:
+  libspectrum_buffer_free( buf );
+  return r;
+}
+
+static test_return_t
+test_79( void )
+{
+  /* libspectrum_buffer_write_dword: value stored little-endian */
+  libspectrum_buffer *buf = libspectrum_buffer_alloc();
+  test_return_t r = TEST_FAIL;
+  const libspectrum_byte *data;
+
+  libspectrum_buffer_write_dword( buf, 0x12345678 );
+
+  if( libspectrum_buffer_get_data_size( buf ) != 4 ) {
+    fprintf( stderr, "%s: test_79: expected size 4, got %lu\n", progname,
+             (unsigned long)libspectrum_buffer_get_data_size( buf ) );
+    goto done;
+  }
+
+  data = libspectrum_buffer_get_data( buf );
+  if( data[0] != 0x78 || data[1] != 0x56 ||
+      data[2] != 0x34 || data[3] != 0x12 ) {
+    fprintf( stderr,
+             "%s: test_79: expected 0x78 0x56 0x34 0x12, got"
+             " 0x%02x 0x%02x 0x%02x 0x%02x\n",
+             progname, data[0], data[1], data[2], data[3] );
+    goto done;
+  }
+
+  r = TEST_PASS;
+
+done:
+  libspectrum_buffer_free( buf );
+  return r;
+}
+
+static test_return_t
+test_80( void )
+{
+  /* libspectrum_buffer_write_buffer: contents of src copied to dest */
+  libspectrum_buffer *src = libspectrum_buffer_alloc();
+  libspectrum_buffer *dest = libspectrum_buffer_alloc();
+  test_return_t r = TEST_FAIL;
+  const libspectrum_byte *data;
+
+  libspectrum_buffer_write_byte( src, 0x01 );
+  libspectrum_buffer_write_byte( src, 0x02 );
+  libspectrum_buffer_write_byte( src, 0x03 );
+
+  libspectrum_buffer_write_buffer( dest, src );
+
+  if( libspectrum_buffer_get_data_size( dest ) != 3 ) {
+    fprintf( stderr, "%s: test_80: expected dest size 3, got %lu\n", progname,
+             (unsigned long)libspectrum_buffer_get_data_size( dest ) );
+    goto done;
+  }
+
+  data = libspectrum_buffer_get_data( dest );
+  if( data[0] != 0x01 || data[1] != 0x02 || data[2] != 0x03 ) {
+    fprintf( stderr,
+             "%s: test_80: expected 0x01 0x02 0x03, got 0x%02x 0x%02x 0x%02x\n",
+             progname, data[0], data[1], data[2] );
+    goto done;
+  }
+
+  r = TEST_PASS;
+
+done:
+  libspectrum_buffer_free( src );
+  libspectrum_buffer_free( dest );
+  return r;
+}
+
+static test_return_t
+test_81( void )
+{
+  /* libspectrum_buffer_is_empty / is_not_empty */
+  libspectrum_buffer *buf = libspectrum_buffer_alloc();
+  test_return_t r = TEST_FAIL;
+
+  if( !libspectrum_buffer_is_empty( buf ) ) {
+    fprintf( stderr, "%s: test_81: new buffer should be empty\n", progname );
+    goto done;
+  }
+  if( libspectrum_buffer_is_not_empty( buf ) ) {
+    fprintf( stderr, "%s: test_81: new buffer should not be non-empty\n",
+             progname );
+    goto done;
+  }
+
+  libspectrum_buffer_write_byte( buf, 0xff );
+
+  if( libspectrum_buffer_is_empty( buf ) ) {
+    fprintf( stderr, "%s: test_81: buffer with data should not be empty\n",
+             progname );
+    goto done;
+  }
+  if( !libspectrum_buffer_is_not_empty( buf ) ) {
+    fprintf( stderr, "%s: test_81: buffer with data should be non-empty\n",
+             progname );
+    goto done;
+  }
+
+  r = TEST_PASS;
+
+done:
+  libspectrum_buffer_free( buf );
+  return r;
+}
+
+static test_return_t
+test_82( void )
+{
+  /* libspectrum_buffer_clear: size resets to zero */
+  libspectrum_buffer *buf = libspectrum_buffer_alloc();
+  test_return_t r = TEST_FAIL;
+
+  libspectrum_buffer_write_byte( buf, 0x42 );
+  libspectrum_buffer_write_byte( buf, 0x43 );
+
+  if( libspectrum_buffer_get_data_size( buf ) != 2 ) {
+    fprintf( stderr, "%s: test_82: expected size 2 before clear, got %lu\n",
+             progname,
+             (unsigned long)libspectrum_buffer_get_data_size( buf ) );
+    goto done;
+  }
+
+  libspectrum_buffer_clear( buf );
+
+  if( libspectrum_buffer_get_data_size( buf ) != 0 ) {
+    fprintf( stderr, "%s: test_82: expected size 0 after clear, got %lu\n",
+             progname,
+             (unsigned long)libspectrum_buffer_get_data_size( buf ) );
+    goto done;
+  }
+  if( !libspectrum_buffer_is_empty( buf ) ) {
+    fprintf( stderr, "%s: test_82: cleared buffer should be empty\n",
+             progname );
+    goto done;
+  }
+
+  r = TEST_PASS;
+
+done:
+  libspectrum_buffer_free( buf );
+  return r;
+}
+
 struct test_description {
 
   test_fn test;
@@ -1099,7 +1306,13 @@ static struct test_description tests[] = {
   { test_73, "Read TZX RAW block edge handling", 0 },
   { test_74, "Trailing pause block TZX file", 0 },
   { test_75, "Read mono WAV threshold fixture", 0 },
-  { test_76, "Read stereo WAV mixdown fixture", 0 }
+  { test_76, "Read stereo WAV mixdown fixture", 0 },
+  { test_77, "Buffer write_byte stores single byte", 0 },
+  { test_78, "Buffer write_word stores little-endian word", 0 },
+  { test_79, "Buffer write_dword stores little-endian dword", 0 },
+  { test_80, "Buffer write_buffer copies src contents to dest", 0 },
+  { test_81, "Buffer is_empty and is_not_empty", 0 },
+  { test_82, "Buffer clear resets data size to zero", 0 }
 };
 
 static size_t test_count = ARRAY_SIZE( tests );
