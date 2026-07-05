91c2c953040a1822a81caa6265a5a2dcaebcc9f4
Fredrick Meunier <fredm@spamcop.net>
2026-07-05 20:14:51 +1000

    Add libspectrum_zx_string_to_utf8() to UTF8 translate ZX Spectrum strings
    This should be useful for the tape browser dialogs or CLI utilities listing
    files in tape, microdrive and disk files.

--- utilities.c
+++ utilities.c
@@ -33,6 +33,50 @@
 
 static const libspectrum_dword tstates_per_ms = TZX_HZ / 1000;
 
+static const char *spectrum_tokens[] = {
+  "SPECTRUM",
+  "PLAY",      "RND",       "INKEY$",    "PI",
+  "FN",        "POINT",     "SCREEN$",   "ATTR",
+  "AT",        "TAB",       "VAL$",      "CODE",
+  "VAL",       "LEN",       "SIN",       "COS",
+  "TAN",       "ASN",       "ACS",       "ATN",
+  "LN",        "EXP",       "INT",       "SQR",
+  "SGN",       "ABS",       "PEEK",      "IN",
+  "USR",       "STR$",      "CHR$",      "NOT",
+  "BIN",       "OR",        "AND",       "<=",
+  ">=",        "<>",        "LINE",      "THEN",
+  "TO",        "STEP",      "DEF FN",    "CAT",
+  "FORMAT",    "MOVE",      "ERASE",     "OPEN #",
+  "CLOSE #",   "MERGE",     "VERIFY",    "BEEP",
+  "CIRCLE",    "INK",       "PAPER",     "FLASH",
+  "BRIGHT",    "INVERSE",   "OVER",      "OUT",
+  "LPRINT",    "LLIST",     "STOP",      "READ",
+  "DATA",      "RESTORE",   "NEW",       "BORDER",
+  "CONTINUE",  "DIM",       "REM",       "FOR",
+  "GO TO",     "GO SUB",    "INPUT",     "LOAD",
+  "LIST",      "LET",       "PAUSE",     "NEXT",
+  "POKE",      "PRINT",     "PLOT",      "RUN",
+  "SAVE",      "RANDOMIZE", "IF",        "CLS",
+  "DRAW",      "CLEAR",     "RETURN",    "COPY"
+};
+
+static const char *graphics_tokens[] = {
+  "\\  ", "\\ '", "\\' ", "\\''",
+  "\\ .", "\\ :", "\\'.", "\\':",
+  "\\. ", "\\.'", "\\: ", "\\:'",
+  "\\..", "\\.:", "\\:.", "\\::"
+};
+
+static char *
+append_bytes( char *ptr, const char *text )
+{
+  size_t length = strlen( text );
+
+  memcpy( ptr, text, length );
+
+  return ptr + length;
+}
+
 libspectrum_dword 
 libspectrum_ms_to_tstates( libspectrum_dword ms )
 {
@@ -67,6 +111,67 @@ size_t
 libspectrum_bits_to_bytes( size_t bits )
 {
   return ( bits + LIBSPECTRUM_BITS_IN_BYTE - 1 ) / LIBSPECTRUM_BITS_IN_BYTE;
+}
+
+char *
+libspectrum_zx_string_to_utf8( const libspectrum_byte *src, size_t length )
+{
+  char *out, *ptr;
+  size_t end, i, allocated;
+  libspectrum_byte b;
+  char udg[3] = "\\a";
+  char ascii[2] = " ";
+
+  if( !src ) return NULL;
+
+  end = length;
+  while( end > 0 && src[ end - 1 ] == ' ' ) end--;
+
+  allocated = end * 10 + 1;
+  out = libspectrum_new( char, allocated );
+  ptr = out;
+
+  for( i = 0; i < end; i++ ) {
+    b = src[ i ];
+
+    switch( b ) {
+    case '\\':
+      ptr = append_bytes( ptr, "\\\\" );
+      break;
+
+    case '^':
+      ptr = append_bytes( ptr, "↑" );
+      break;
+
+    case '`':
+      ptr = append_bytes( ptr, "£" );
+      break;
+
+    case 127:
+      ptr = append_bytes( ptr, "©" );
+      break;
+
+    default:
+      if( b >= 32 && b < 127 ) {
+        ascii[0] = b;
+        ptr = append_bytes( ptr, ascii );
+      } else if( b >= 128 && b <= 143 ) {
+        ptr = append_bytes( ptr, graphics_tokens[ b - 128 ] );
+      } else if( b >= 144 && b <= 164 ) {
+        udg[1] = 'a' + b - 144;
+        ptr = append_bytes( ptr, udg );
+      } else if( b >= 165 ) {
+        ptr = append_bytes( ptr, spectrum_tokens[ b - 165 ] );
+      } else {
+        ptr = append_bytes( ptr, "?" );
+      }
+      break;
+    }
+  }
+
+  *ptr = '\0';
+
+  return libspectrum_renew( char, out, ptr - out + 1 );
 }
 
 char*
